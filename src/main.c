3259ed390b3f41e9149fd4da7b798e3ead365e26
bulka <jej07267@gmail.com>
2024-09-12 06:43:59 +0300

    Dev 0.01 build 72

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "lib/types.h"
 #include "lib/memory.h"
 
@@ -16,17 +18,43 @@
 #define DS      0x16
 #define CS      0x17
 
+// 4KB of memory allocated as default
+i32 MEMSIZE = 4096;
+
 i32 main(i32 argc, char** argv) {
-	if (argc > 3) {
-		fprintf(stderr, "ERROR: expected only 1 argument\n");
-		fprintf(stderr, "SYNTAX: bvim <file>");
-		return 1;
+	char* filename = argv[1];
+	for (char i = 2; i < argc; i++) {
+		if (!strcmp(argv[i], "-h") || !strcmp(argv[i], "-?") || !strcmp(argv[i], "--help")) {
+			printf("SYNTAX: bvim <file> [options]\n");
+			printf("  Options:\n");
+			printf("    -MB 4096         Set memory size (bytes)\n");
+			printf("    -MK 8            Set memory size (kilobytes)\n");
+			printf("    -MM 64           Set memory size (megabytes)\n");
+			printf("    -h, -?, --help   Show help\n");
+			return 2;
+		}
+		else if (!strcmp(argv[i], "-MB")) {
+			MEMSIZE = atoi(argv[i+1]);
+			i += 2;
+		}
+		else if (!strcmp(argv[i], "-MK")) {
+			MEMSIZE = atoi(argv[i+1])*1024;
+			i += 2;
+		}
+		else if (!strcmp(argv[i], "-MM")) {
+			MEMSIZE = atoi(argv[i+1])*1048576;
+			i += 2;
+		}
+		else {
+			printf("%s\n", argv[i]);
+		}
 	}
 
-	FILE* srcfile = fopen(argv[1], "rb");
+	FILE* srcfile = fopen(filename, "rb");
 	char src[READSZ];
 
 	char memory[4096];
+	char modal = 0x00;
 	i32 regs[0x30];
 
 	fread(src, 1, READSZ, srcfile); // Load the file
@@ -34,53 +62,72 @@ i32 main(i32 argc, char** argv) {
 	memlod(memory, src, 0x7F); // Move to memory until 0x7F
 
 	while (true) {
-		switch (memory[regs[IP]]) {
-			case 0x00:
-				printf("NOP\n");
-				break;
-			case 0x01:
-				regs[memory[regs[IP]+1]] += regs[memory[regs[IP]+2]];
-				regs[IP] += 2;
-				break;
-			case 0x02:
-				regs[memory[regs[IP]+1]] -= regs[memory[regs[IP]+2]];
-				regs[IP] += 2;
-				break;
-			case 0x03:
-				regs[memory[regs[IP]+1]] *= regs[memory[regs[IP]+2]];
-				regs[IP] += 2;
-				break;
-			case 0x04:
-				regs[memory[regs[IP]+3]] = regs[memory[regs[IP]+1]] *= regs[memory[regs[IP]+2]];
-				regs[IP] += 3;
-				break;
-			case 0x05:
-				regs[memory[regs[IP]+3]] = regs[memory[regs[IP]+1]] /= regs[memory[regs[IP]+2]];
-				regs[IP] += 3;
-				break;
-			case 0x06:
-				regs[memory[regs[IP]+1]] += regs[memory[regs[IP]+2]];
-				regs[IP] += 2;
-				break;
-			case 0x07:
-				regs[memory[regs[IP]+1]] = (memory[regs[IP]+2] << 8) + (memory[regs[IP]+3]);
-				regs[IP] += 3;
-				break;
-			case 0x20:
-				if (regs[0x07] == 0) {
-					return regs[0x08];
-				}
-				else {
-					fprintf(stderr, "Unkown system call %02x", regs[7]);
-				}
-			case 0x7F:
-				printf("EOF\n");
-				goto progEOF;
+		if (regs[IP] >= MEMSIZE) {
+			fprintf(stderr, "Memory end\n");
+			return 4;
+		}
+		if (modal == 0x00) { // If executing code
+			switch (memory[regs[IP]]) {
+				case 0x00:
+					printf("NOP\n");
+					break;
+				case 0x01:
+					regs[memory[regs[IP]+1]] += regs[memory[regs[IP]+2]];
+					regs[IP] += 2;
+					break;
+				case 0x02:
+					regs[memory[regs[IP]+1]] -= regs[memory[regs[IP]+2]];
+					regs[IP] += 2;
+					break;
+				case 0x03:
+					regs[memory[regs[IP]+1]] *= regs[memory[regs[IP]+2]];
+					regs[IP] += 2;
+					break;
+				case 0x04:
+					regs[memory[regs[IP]+3]] = regs[memory[regs[IP]+1]] *= regs[memory[regs[IP]+2]];
+					regs[IP] += 3;
+					break;
+				case 0x05:
+					regs[memory[regs[IP]+3]] = regs[memory[regs[IP]+1]] /= regs[memory[regs[IP]+2]];
+					regs[IP] += 3;
+					break;
+				case 0x06:
+					regs[memory[regs[IP]+1]] += regs[memory[regs[IP]+2]];
+					regs[IP] += 2;
+					break;
+				case 0x07:
+					regs[memory[regs[IP]+1]] = (memory[regs[IP]+2] << 8) + (memory[regs[IP]+3]);
+					regs[IP] += 3;
+					break;
+				case 0x08:
+					regs[memory[regs[IP]+1]] = (memory[regs[IP]+2] << 24) + (memory[regs[IP]+3] << 16) + (memory[regs[IP]+4] << 8) + (memory[regs[IP]+5]);
+					regs[IP] += 5;
+					break;
+				case 0x20:
+					switch (regs[0x07]) {
+						case 0x00:
+							return regs[0x08];
+							break;
+						case 0x01:
+							if (regs[0x08] == 1) {
+								putc(regs[0x09], stdout);
+							}
+							else if (regs[0x08] == 2) {
+								putc(regs[0x09], stderr);
+							}
+							break;
+					default:
+						fprintf(stderr, "Unkown system call %02x", regs[0x07]);
+					}
+					break;
+				case 0x7F:
+					goto progEOF;
+			}
 		}
 		regs[IP]++;
 	}
 	progEOF:
-		printf("EOF\n");
+		printf("\nFFFh\n");
 
 	return 0;
 }
